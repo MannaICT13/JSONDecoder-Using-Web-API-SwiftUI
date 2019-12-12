9a14e37b056f9f932abf896c7e59a831cbb011d7
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2019-12-12 20:01:38 +0600

    Added JsonStruct Structure

--- JSONEncoder Using Web API SwiftUI/ContentView.swift
+++ JSONEncoder Using Web API SwiftUI/ContentView.swift
@@ -9,12 +9,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello World")
+        
+      Text("Hello World")
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
---
+++ JSONEncoder Using Web API SwiftUI/JsonStruct.swift
@@ -0,0 +1,18 @@
+//
+//  JsonStruct.swift
+//  JSONEncoder Using Web API SwiftUI
+//
+//  Created by Md Khaled Hasan Manna on 12/12/19.
+//  Copyright © 2019 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import Foundation
+
+struct JsonStruct {
+    
+    let name : String
+    let capital : String
+    let region : String
+    
+    
+}
